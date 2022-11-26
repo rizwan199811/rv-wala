85f20e24b6291aa8aeeb44bc8b3b0251436c18f1
AnusMurtaza <anas.aq97@gmail.com>
2022-11-26 15:30:34 +0500

    booking

--- src/views/Booking.js
+++ src/views/Booking.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardBody, CardTitle, CardSubtitle, Table } from "reactstrap";
+import { Card, CardBody, CardTitle, CardSubtitle, Table, Button } from "reactstrap";
 import user1 from "../assets/images/users/user1.jpg";
 import user2 from "../assets/images/users/user2.jpg";
 import user3 from "../assets/images/users/user3.jpg";
@@ -9,49 +9,14 @@ import user5 from "../assets/images/users/user5.jpg";
 const tableData = [
   {
     avatar: user1,
-    name: "Hanna Gover",
+    booking: "#791",
     email: "hgover@gmail.com",
-    project: "Flexy React",
-    status: "pending",
-    weeks: "35",
+    project: "2009 R-POD",
+    user: "Jessica",
+    weeks: "August 22, 2022 - August 24, 2022",
     budget: "95K",
   },
-  {
-    avatar: user2,
-    name: "Hanna Gover",
-    email: "hgover@gmail.com",
-    project: "Lading pro React",
-    status: "done",
-    weeks: "35",
-    budget: "95K",
-  },
-  {
-    avatar: user3,
-    name: "Hanna Gover",
-    email: "hgover@gmail.com",
-    project: "Elite React",
-    status: "holt",
-    weeks: "35",
-    budget: "95K",
-  },
-  {
-    avatar: user4,
-    name: "Hanna Gover",
-    email: "hgover@gmail.com",
-    project: "Flexy React",
-    status: "pending",
-    weeks: "35",
-    budget: "95K",
-  },
-  {
-    avatar: user5,
-    name: "Hanna Gover",
-    email: "hgover@gmail.com",
-    project: "Ample React",
-    status: "done",
-    weeks: "35",
-    budget: "95K",
-  },
+
 ];
 const Booking = () => {
   return (
@@ -64,44 +29,30 @@ const Booking = () => {
           <Table className="no-wrap mt-3 align-middle" responsive borderless>
             <thead>
               <tr>
-                <th>Team Lead</th>
-                <th>Project</th>
+                <th>Bookings</th>
+                <th>Listing</th>
 
+                <th>User</th>
+                <th>Dates</th>
                 <th>Status</th>
-                <th>Weeks</th>
-                <th>Budget</th>
+                <th>Action</th>
               </tr>
             </thead>
             <tbody>
               {tableData.map((tdata, index) => (
                 <tr key={index} className="border-top">
-                  <td>
-                    <div className="d-flex align-items-center p-2">
-                      <img
-                        src={tdata.avatar}
-                        className="rounded-circle"
-                        alt="avatar"
-                        width="45"
-                        height="45"
-                      />
-                      <div className="ms-3">
-                        <h6 className="mb-0">{tdata.name}</h6>
-                        <span className="text-muted">{tdata.email}</span>
-                      </div>
-                    </div>
-                  </td>
+
+                  <td>{tdata.booking}</td>
                   <td>{tdata.project}</td>
-                  <td>
-                    {tdata.status === "pending" ? (
-                      <span className="p-2 bg-danger rounded-circle d-inline-block ms-3"></span>
-                    ) : tdata.status === "holt" ? (
-                      <span className="p-2 bg-warning rounded-circle d-inline-block ms-3"></span>
-                    ) : (
-                      <span className="p-2 bg-success rounded-circle d-inline-block ms-3"></span>
-                    )}
-                  </td>
+                  <td>{tdata.user}</td>
                   <td>{tdata.weeks}</td>
-                  <td>{tdata.budget}</td>
+                  <td>
+                  <Button className="btn me-2" outline color="success" size="sm" >Confirmed</Button>
+                  </td>
+                  <td>
+                  <Button className="btn me-2" color="success" size="sm" >Confirm</Button>
+                  <Button className="btn" color="danger" size="sm"> Decline</Button>
+                  </td>
                 </tr>
               ))}
             </tbody>
