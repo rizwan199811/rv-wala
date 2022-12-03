9b0c68da877a26c70aaf228d73677d0446dc7fc5
devAvanza <rizwan.ahmed@avanzainnovations.com>
2022-12-04 02:05:15 +0500

    changes done

--- src/config/apiURL.js
+++ src/config/apiURL.js
@@ -3,7 +3,7 @@
 // const  stagingURL="https://rv-wala.herokuapp.com/api";
 // const localURL="http://localhost:4000/api";
 // const baseURL = "http://localhost:4000/api";
-const baseURL = "https://rv-wala.herokuapp.com/api";
+const baseURL = "http://3.99.168.68:4000/api";
 export {
     baseURL
 }
